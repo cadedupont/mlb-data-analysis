7b4b0462dc3e41871d4bb7c263ff70f5d9931681
Cade DuPont <cadedupont45@gmail.com>
2024-02-01 10:39:08 -0600

    Fix incorrect ERA calculation, some pitchers being overcounted

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# mlb-data-analysis
-Performing analysis on dataset of active MLB players in R
+# MLB Data Analysis
+
+Project intended to familiarize myself with data analysis in R. The data used is from the [Lahman database](https://cran.r-project.org/web/packages/Lahman/Lahman.pdf), which contains a wide variety of statistics for Major League Baseball (MLB).
+
+## `era_vs_age.R`
+
+This script creates a scatter plot of the earned run average (ERA) of MLB pitchers against their age in the 2022 season. The data utilizes the `Pitching` table left-joined with the `People` table in the database to get the age of the pitchers.
+
+To be qualified for the plot, a pitcher must have thrown at least 100 innings in the season and played in a minimum of 20 games. This is to ensure that the pitcher had a significant amount of playing time in the season (i.e. ignore position players that have pitched, pitchers that were injured, etc.).
--- era_vs_age.r
+++ era_vs_age.r
@@ -10,26 +10,28 @@ library(ggplot2)
 library(Lahman)
 library(ggrepel)
 
-# Define minimum number of innings pitched
-# Used to filter out position players that have pitched
-minInnings <- 50
+# Set minimum innings pitched and games played to be considered an active pitcher
+minInnings <- 100
+minGames <- 20
+
+# Get the latest year in the Lahman database
 latestYear <- max(Lahman::Pitching$yearID)
 
-# Get ERA and age of active pitchers in the latest year
-# Filter out players with less than 50 innings pitched
+# Get the data for active pitchers in the latest year
 pitchers <- Lahman::Pitching %>%
   filter(yearID == latestYear) %>%
-  group_by(playerID) %>%
-  summarise(
-    IP = sum(IPouts) / 3, 
-    ERA = mean(ERA, na.rm = TRUE)
-  ) %>%
-  filter(IP >= minInnings) %>%
   left_join(Lahman::People, by = "playerID") %>%
-  mutate(age = latestYear - birthYear) %>%
-  select(playerID, nameFirst, nameLast, age, ERA)
+  mutate(age = yearID - birthYear) %>%
+  select(playerID, nameFirst, nameLast, yearID, age, ERA, ER, IPouts, G) %>%
+  group_by(playerID, yearID, nameFirst, nameLast, age, G) %>%
+  summarise(
+    IP = sum(IPouts) / 3,
+    ER = sum(ER),
+    ERA = (ER / IP) * 9
+  ) %>%
+  filter(IP >= minInnings & G >= minGames)
 
-# Plot ERA vs. age, label points with player names if they're standing out
+# Create a scatter plot of ERA vs. age for active pitchers
 ggplot(pitchers, aes(x = age, y = ERA)) +
   geom_point() +
   geom_smooth(method = "lm", se = FALSE, color = "red") +
